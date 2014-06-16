5bc4a585dba42182b34732093c12c462172ac7bc
Simon Tao <simontaotw@gmail.com>
2014-06-15 18:36:42 -0700

    Add jsonp

--- main.py
+++ main.py
@@ -20,36 +20,40 @@ from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 import webapp2
 
-CACHE_EXPIRATION = 60*60*24 # 1 day
+CACHE_EXPIRATION = 60 * 60 * 24  # 1 day
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         url = self.request.get('url')
+        callback = self.request.get('callback', None)
         if not url.startswith('http://'):
-            url = 'http://%s' %url 
-        
+            url = 'http://%s' % url
+
         content = memcache.get(url)
         headers = memcache.get('%s:headers' % url)
         status = memcache.get('%s:status' % url)
         if content and headers and status:
             logging.info('cache hit')
-            self.generate_response(content, headers, status)
+            self.generate_response(content, headers, status, callback)
         else:
             logging.info('cache miss')
             result = urlfetch.fetch(url, deadline=60)
             if result.status_code != 200:
                 result.content = 'Cannot fetch %s' % url
-            
-            self.generate_response(result.content, result.headers, result.status_code)
-            
+
+            self.generate_response(result.content, result.headers, result.status_code, callback)
+
             memcache.set(url, result.content, CACHE_EXPIRATION)
             memcache.set('%s:headers' % url, result.headers, CACHE_EXPIRATION)
             memcache.set('%s:status' % url, result.status_code, CACHE_EXPIRATION)
 
 
-    def generate_response(self, content, headers, status):
-        self.response.write(content)
+    def generate_response(self, content, headers, status, callback=None):
+        if callback is not None:
+            self.response.write(callback + '(' + content + ')')
+        else:
+            self.response.write(content)
         for key, value in headers.items():
             self.response.headers.add_header(key, value)
         self.response.set_status(status)
